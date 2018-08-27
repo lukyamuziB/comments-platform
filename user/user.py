669153065c477efe0815f39ded6a43167adce0a1
Songoi Stephen <stewason@gmail.com>
2018-08-27 13:36:34 +0300

    [#160056976] Set up user object

---
+++ user/user.py
@@ -0,0 +1,8 @@
+user_list = []
+
+class User(object):
+    def __init__(self, data={}):
+        self.username = data.get('username')
+        self.usermail = data.get('usermail')
+        self.userpassword = data.get("userpassword")
+        self.permission = data.get('permission')
