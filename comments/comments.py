076560039c54881c1c2a5d469e6ff6092d145d8c
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-27 14:11:52 +0300

    update comment class

--- comments/comments.py
+++ comments/comments.py
@@ -5,7 +5,7 @@ this module is inteded to hold comments class
 class Comment(object):
     """comment class"""
 
-    def __init__(self,comment):
+    def __init__(self,comment,list):
         """
         init comment attribute
         """
@@ -13,3 +13,21 @@ class Comment(object):
         self.body = comment["body"]
         self.timestamp=comment["timestamp"]
         self.author=comment["author"]
+
+    def delete_comment(self,comment_list,id):
+        """delete a comment in comments list"""
+        comment=[c for c in comment_list if c["id"]==id]
+        comment_list.remove(comment[0])
+        return comment_list
+
+    def edit_comment(self,comment_list,id):
+        """edit comment"""
+        comment=[c for c in comment_list if c["id"]==id]
+        body=input("please enter comment")
+
+        if body or body == "" or body == " ":
+            comment[0]["body"]=body
+            comment = Comment(data
+            return comment
+            print ("body can't be empty")
+        return comment_list
