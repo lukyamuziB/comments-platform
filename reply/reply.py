9bcfe94c2a60c7766728d5c7f2aed5c75c85a064
winniejerop <winniecherop@outlook.com>
2018-08-27 13:42:34 +0300

    ft-[#160055619] comment as a reply

---
+++ reply/reply.py
@@ -0,0 +1,15 @@
+'''
+This class handles the comment as a reply
+'''
+class Reply(object):
+
+    def __init__(self,body,id,reply_id):
+        self.reply_id = reply_id
+        self.body = body
+        self.id = id
+
+
+
+
+
+
